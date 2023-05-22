8f7e10a9ee49725aeb5be8792bfbad9dbce29be0
TowmirAtInsta <towmir@instawebworks.com.au>
2023-05-22 13:49:55 +0600

    added nav link items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,35 @@
 import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
+import { useRouter } from "next/router";
+
+const CustomLink = ({ title, href, className = "" }) => {
+  const router = useRouter();
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+
+      <span
+        className={`h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 const Navbar = () => {
   return (
-    <header className="w-full px-32 py-8 font-medium">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/blogs">Blogs</Link>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/blogs" title="Blogs" className="ml-4" />
       </nav>
-      <h2>Logo</h2>
+
       <nav>
         <Link href="/" target="_blank">
           T
@@ -28,6 +47,10 @@ const Navbar = () => {
           T
         </Link>
       </nav>
+
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
     </header>
   );
 };
